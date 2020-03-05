fa5bfe209c95a10bf3a712698b6e76ae3ade3544
Johan Reinalda <johan@reinalda.net>
2020-03-05 15:49:36 -0800

    v1.1.7

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2019-2020, Various'
 author = 'Various'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.1.6 (2020-02-27)'
+release = 'v1.1.7 (2020-03-05)'
 
 
 # -- General configuration ---------------------------------------------------
--- openl2m/openl2m/settings.py
+++ openl2m/openl2m/settings.py
@@ -48,8 +48,8 @@ except ImportError:
     )
 
 # if you change this version, also change it in docs/conf.py and docs/releases/<version> !!!
-VERSION = '1.1.6'
-VERSION_DATE = '2020-02-27'
+VERSION = '1.1.7'
+VERSION_DATE = '2020-03-05'
 
 # Hostname
 HOSTNAME = platform.node()
