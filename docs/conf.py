714ea2436028ffb0b1da39a2f1d9454202338e2d
Johan Reinalda <johan@reinalda.net>
2020-11-12 18:39:24 -0500

    version update to v1.1.13

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2019-2020, Various'
 author = 'Various'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.1.12 (2020-07-20)'
+release = 'v1.1.13 (2020-11-12)'
 
 # -- General configuration ---------------------------------------------------
 
