4ab3643cbd549deee64bab16790e175dd87c4d5a
Johan Reinalda <johan@reinalda.net>
2021-09-03 14:54:55 -0400

    update connector types

--- openl2m/switches/connect/connect.py
+++ openl2m/switches/connect/connect.py
@@ -23,7 +23,7 @@ from switches.connect.connector import *
 from switches.connect.classes import Error
 import switches.views
 
-# here are the SNMP specific classes:
+# here are the device specific classes.
 # this should be made dynamic at some point!
 from switches.connect.snmp.connector import *
 from switches.connect.snmp.constants import *
@@ -58,63 +58,68 @@ def get_connection_object(request, group, switch):
     """
     dprint(f"get_connection_object() for {switch} at {datetime.datetime.now()}")
 
-    # hardcode the dummy switch :-)
-    if switch.name.lower() == "dummy":
-        connection = DummyConnector(request, group, switch)
-    else:
-        if switch.connector_type == CONNECTOR_TYPE_SNMP:
-            if not switch.snmp_oid:
-                # we don't know this switch yet, go probe it
-                conn = SnmpConnector(request, group, switch)
-                if not conn._probe_mibs():
-                    raise Exception('Error probing device. Is the SNMP Profile correct?')
-                    return  # for clarify
+    # What type of connector are we using?
+    if switch.connector_type == CONNECTOR_TYPE_SNMP:
+        if not switch.snmp_oid:
+            # we don't know this switch yet, go probe it
+            conn = SnmpConnector(request, group, switch)
+            if not conn._probe_mibs():
+                raise Exception('Error probing device. Is the SNMP Profile correct?')
+                return  # for clarify
 
-            # now we should have the basics:
-            if switch.snmp_oid:
-                # we have the ObjectID, what kind of vendor is it:
-                dprint(f"   Checking device type for {switch.snmp_oid}")
-                sub_oid = oid_in_branch(enterprises, switch.snmp_oid)
-                if sub_oid:
-                    parts = sub_oid.split('.', 1)  # 1 means one split, two elements!
-                    enterprise_id = int(parts[0])
-                    # here we go:
-                    if enterprise_id == ENTERPRISE_ID_CISCO:
-                        connection = SnmpConnectorCisco(request, group, switch)
+        # now we should have the basics:
+        if switch.snmp_oid:
+            # we have the ObjectID, what kind of vendor is it:
+            dprint(f"   Checking device type for {switch.snmp_oid}")
+            sub_oid = oid_in_branch(enterprises, switch.snmp_oid)
+            if sub_oid:
+                parts = sub_oid.split('.', 1)  # 1 means one split, two elements!
+                enterprise_id = int(parts[0])
+                # here we go:
+                if enterprise_id == ENTERPRISE_ID_CISCO:
+                    connection = SnmpConnectorCisco(request, group, switch)
 
-                    elif enterprise_id == ENTERPRISE_ID_JUNIPER:
-                        connection = SnmpConnectorJuniper(request, group, switch)
+                elif enterprise_id == ENTERPRISE_ID_JUNIPER:
+                    connection = SnmpConnectorJuniper(request, group, switch)
 
-                    elif enterprise_id == ENTERPRISE_ID_HP:
-                        connection = SnmpConnectorProcurve(request, group, switch)
+                elif enterprise_id == ENTERPRISE_ID_HP:
+                    connection = SnmpConnectorProcurve(request, group, switch)
 
-                    elif enterprise_id == ENTERPRISE_ID_H3C:
-                        connection = SnmpConnectorComware(request, group, switch)
+                elif enterprise_id == ENTERPRISE_ID_H3C:
+                    connection = SnmpConnectorComware(request, group, switch)
 
-                    elif enterprise_id == ENTERPRISE_ID_HP_ENTERPRISE:
-                        connection = SnmpConnectorAruba(request, group, switch)
+                elif enterprise_id == ENTERPRISE_ID_HP_ENTERPRISE:
+                    connection = SnmpConnectorAruba(request, group, switch)
 
-                    # Dell is yet to be tested!
-                    # elif enterprise_id == ENTERPRISE_ID_DELL:
-                    #    connection = SnmpConnectorDell(request, group, switch)
+                # Dell is yet to be tested!
+                # elif enterprise_id == ENTERPRISE_ID_DELL:
+                #    connection = SnmpConnectorDell(request, group, switch)
 
-                    else:
-                        # system oid found, but unknown vendor:
-                        connection = SnmpConnector(request, group, switch)
+                else:
+                    # system oid found, but unknown vendor:
+                    connection = SnmpConnector(request, group, switch)
 
-            # no system oid found, return a "generic" SNMP object
-            else:
-                connection = SnmpConnector(request, group, switch)
-
-        elif switch.connector_type == CONNECTOR_TYPE_AOSCX:
-            connection = AosCxConnector(request, group, switch)
-
-        elif switch.connector_type == CONNECTOR_TYPE_NAPALM:
-            connection = NapalmConnector(request, group, switch)
-
+        # no system oid found, return a "generic" SNMP object
         else:
-            # should not happen!
-            raise Exception("Invalid connector type configured on switch!")
+            connection = SnmpConnector(request, group, switch)
+
+    # This is the "custom" Aruba AOS CS connector, using the device REST API.
+    # this implementation is planned, ETA is TBD :-)
+    elif switch.connector_type == CONNECTOR_TYPE_AOSCX:
+        connection = AosCxConnector(request, group, switch)
+
+    # The Napalm connector uses the Python Napalm package. It is read-only.
+    # Mostly implemented to test and show the new Connector() API class.
+    elif switch.connector_type == CONNECTOR_TYPE_NAPALM:
+        connection = NapalmConnector(request, group, switch)
+
+    # this is a test class, to show and test the new Connector() API class:
+    elif switch.connector_type == CONNECTOR_TYPE_TESTDUMMY:
+        connection = DummyConnector(request, group, switch)
+
+    else:
+        # should not happen!
+        raise Exception("Invalid connector type configured on switch!")
 
     # load caches (http session, memory cache (future), whatever else for performance)
     connection.load_cache()
