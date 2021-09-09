f49f9c5eee22cfa8178921185bc570a12fbe438c
Johan Reinalda <johan@reinalda.net>
2021-09-09 13:54:26 -0400

    change version to 2.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2019-2021, Various'
 author = 'Various'
 
 # The full version, including alpha/beta/rc tags
-release = 'v2.0.0 (2021-09-02)'
+release = 'v2.0.1 (2021-09-09)'
 
 # -- General configuration ---------------------------------------------------
 
--- openl2m/openl2m/settings.py
+++ openl2m/openl2m/settings.py
@@ -49,8 +49,8 @@ except ImportError:
     )
 
 # if you change this version, also change it in docs/conf.py and docs/releases/<version> !!!
-VERSION = '2.0.0'
-VERSION_DATE = '2021-09-02'
+VERSION = '2.0.1'
+VERSION_DATE = '2021-09-09'
 
 # Hostname
 HOSTNAME = platform.node()
