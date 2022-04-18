5b6b7b01a8a08133d8048ead5e311578b9d49bec
Johan Reinalda <johan@reinalda.net>
2022-04-18 18:24:12 -0400

    v2.2dev2 release

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2019-2022, Various'
 author = 'Various'
 
 # The full version, including alpha/beta/rc tags
-release = 'v2.2-dev1 (2022-02-04)'
+release = 'v2.2-dev2 (2022-04-18)'
 
 # -- General configuration ---------------------------------------------------
 
