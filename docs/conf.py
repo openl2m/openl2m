0d400c0b9133794ca8ed96e747dff8cd6a769387
Johan Reinalda <johan@reinalda.net>
2022-06-15 13:31:07 -0700

    fix fontawesome reference

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ html_static_path = ['_static']
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
-    '/static/fontawesome-5.15.3/css/all.css',
+    '/static/fontawesome-5.15.4/css/all.css',
 ]
 
 # html_js_files = [
