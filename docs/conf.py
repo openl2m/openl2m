88bafb7e1e024185ccf06f82f163da3120e48b33
Johan Reinalda <johan@reinalda.net>
2022-10-14 16:32:39 -0700

    release v2.3.2

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2019-2022, Various'
 author = 'Various'
 
 # The full version, including alpha/beta/rc tags
-release = 'v2.3.1 (2022-10-06)'
+release = 'v2.3.2 (2022-10-14)'
 
 # -- General configuration ---------------------------------------------------
 
--- openl2m/openl2m/settings.py
+++ openl2m/openl2m/settings.py
@@ -49,8 +49,8 @@ except ImportError:
     )
 
 # if you change this version, also change it in docs/conf.py and docs/releases/<version> !!!
-VERSION = '2.3.1'
-VERSION_DATE = '2021-10-06'
+VERSION = '2.3.2'
+VERSION_DATE = '2021-10-14'
 
 # Hostname
 HOSTNAME = platform.node()
