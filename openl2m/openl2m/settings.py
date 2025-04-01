d6b1b805be050a4cd7525a1bfe05ca339b64510e
Johan Reinalda <johan@reinalda.net>
2025-04-01 12:26:21 -0700

    v3.4.1

--- openl2m/openl2m/settings.py
+++ openl2m/openl2m/settings.py
@@ -52,8 +52,8 @@ except ImportError:
     )
 
 # if you change this version, also change it in docs/conf.py and docs/releases/<version> !!!
-VERSION = "3.4"
-VERSION_DATE = "2025-02-10"
+VERSION = "3.4.1"
+VERSION_DATE = "2025-04-02"
 
 # Hostname
 HOSTNAME = platform.node()
