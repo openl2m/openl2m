9ed29450b2fc4fb8beede643aba97b850c65b262
Johan Reinalda <johan@reinalda.net>
2026-06-30 15:29:03 -0700

    fix port vlan change regression bug

--- openl2m/switches/connect/snmp/arista_eos/connector.py
+++ openl2m/switches/connect/snmp/arista_eos/connector.py
@@ -170,7 +170,7 @@ class SnmpConnectorAristaEOS(SnmpConnector):
             oid=f"{dot1qPvid}.{interface.port_id}",
             value=int(new_vlan_id),
             snmp_type="u",
-            parser=self._parse_mibs_vlan_related,
+            parser=self._parse_mibs_vlan_dot1q_pvid,
         ):
             return False
 
